8eb090d3bb892f7b88dfd38cbc7ac726b27bf9fc
ashugh12 <ashutoshmishra61679@gmail.com>
2024-06-19 23:09:18 +0530

    Character hasing

--- check.cpp
+++ check.cpp
@@ -6,22 +6,35 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
-    int n;
-    cin>>n;
-    int arr[n]; 
-    for(int i=0;i<n; i++){
-        cin>>arr[i];
-    }
-    int hash[13]={0};
-    for(int i=0; i<n; i++){
-        hash[arr[i]]+=1;
+    // int n;
+    // cin>>n;
+    // int arr[n]; 
+    // for(int i=0;i<n; i++){
+    //     cin>>arr[i];
+    // }
+    // int hash[13]={0};
+    // for(int i=0; i<n; i++){
+    //     hash[arr[i]]+=1;
+    // }
+    // int q;
+    // cin>>q;
+    // while(q--){
+    //     int number;
+    //     cin>>number;
+    //     cout<<hash[number]<<endl;
+    // }
+    // return 0;
+
+    string s="abskckddslds";
+    int hash[26]={0};
+    for(int i=0; i<s.size(); i++){
+        hash[s[i]-'a']++; 
     }
     int q;
-    cin>>q;
+    cin>>q; 
     while(q--){
-        int number;
-        cin>>number;
-        cout<<hash[number]<<endl;
+        char c;
+        cin>>c;
+        cout<<hash[c-'a']<<endl;
     }
-    return 0;
 }
